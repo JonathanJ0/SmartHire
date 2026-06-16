f1391872c22fb0e580ea4ee20835c12d9d92b042
JonathanJ0 <128973616+JonathanJ0@users.noreply.github.com>
2026-06-16 13:06:47 +0530

    Delete SmartHire directory

--- SmartHire/my-app/app/interviewer/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import { SessionDetailClient } from "@/components/interviewer/SessionDetailClient";
-
-type Props = { params: Promise<{ id: string }> };
-
-export default async function SessionDetailPage({ params }: Props) {
-  const { id } = await params;
-  return <SessionDetailClient sessionId={id} />;
-}
